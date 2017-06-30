c7fc45c22ede9bb6e26c8e825d672fa20321a2e3
NoahVlncrt <noah.vaillancourt@gmail.com>
2017-06-29 20:51:39 -0400

    Tags exist now. They don't do much but they are there

--- src/Uploader/submitted.js
+++ src/Uploader/submitted.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+import { Button, ButtonGroup } from 'reactstrap';
+
 export default class Submitted extends Component {
   constructor(props){
     super(props)
-    console.log(this.props.palette)
+    this.state = {cSelected: []}
   }
+
+  onCheckboxBtnClick(selected) {
+    const index = this.state.cSelected.indexOf(selected);
+    if (index < 0) {
+      this.state.cSelected.push(selected);
+    } else {
+      this.state.cSelected.splice(index, 1);
+    }
+    this.setState({ cSelected: [...this.state.cSelected] });
+  }
+
+
 
   render(){
     const Styles = {
@@ -12,7 +26,7 @@ export default class Submitted extends Component {
         float: "left",
         width: "25%"
       },
-      "colorSwatches": {
+      "flexCenter": {
         display: "flex",
         justifyContent: "center"
       }
@@ -21,7 +35,7 @@ export default class Submitted extends Component {
       <div>
         <img src={this.props.imgLink} alt="" style={Styles.imgArea}/>
         <h2 style={{textAlign: "center"}}>Colors</h2>
-        <div style={Styles.colorSwatches}>
+        <div style={Styles.flexCenter}>
           <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.Vibrant.getHex()}}></div>
           <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.LightVibrant.getHex()}}></div>
           <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.DarkVibrant.getHex()}}></div>
@@ -30,6 +44,14 @@ export default class Submitted extends Component {
           <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.DarkMuted.getHex()}}></div>
         </div>
         <h2 style={{textAlign: "center"}}>Tags</h2>
+        <div style={Styles.flexCenter}>
+          <ButtonGroup>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(1)} active={this.state.cSelected.includes(1)}>Shorts</Button>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.cSelected.includes(2)}>Pants</Button>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.cSelected.includes(3)}>Shirt</Button>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(4)} active={this.state.cSelected.includes(4)}>Jacket</Button>
+          </ButtonGroup>
+        </div>
       </div>
     )
   }
