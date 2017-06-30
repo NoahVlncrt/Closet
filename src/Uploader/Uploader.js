03f05499b8c11585378c26d6837b2177af38a414
NoahVlncrt <noah.vaillancourt@gmail.com>
2017-06-29 20:39:12 -0400

    WE GOT COLOR HUNTING WORKING! As well as an almost done upload ui.

--- src/Uploader/Uploader.js
+++ src/Uploader/Uploader.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
+import * as Vibrant from 'node-vibrant'
 
 import NewNavbar from "../components/Navbar";
 import Fresh from './fresh.js';
-import Submitted from './submitted.js';
+import Submitted from './submitted.js'
 
 export default class Uploader extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      imgLink: ''
+      imgLink: '',
+      palette: ''
     }
   }
 
   onImageDrop = (acceptedFiles, rejectedFiles) => {
-    this.setState({imgLink: acceptedFiles[0].preview})
+    Vibrant.from(acceptedFiles[0].preview).getPalette((err, palette) => {this.setState({palette: palette}); this.setState({imgLink: acceptedFiles[0].preview})})
   }
+
 
   LogicalDecider = () => {
     if(this.state.imgLink === ''){
       return <Fresh update={this.onImageDrop.bind(this)}/>
     } else {
-      return <Submitted imgLink={this.state.imgLink}/>
+      return <Submitted imgLink={this.state.imgLink} palette={this.state.palette}/>
     }
   }
 
--- src/Uploader/submitted.js
+++ src/Uploader/submitted.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 export default class Submitted extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.props.palette)
+  }
+
   render(){
+    const Styles = {
+      "imgArea": {
+        float: "left",
+        width: "25%"
+      },
+      "colorSwatches": {
+        display: "flex",
+        justifyContent: "center"
+      }
+    }
     return (
       <div>
-        <img src={this.props.imgLink} alt="" style={{width: "30%"}}/>
+        <img src={this.props.imgLink} alt="" style={Styles.imgArea}/>
+        <h2 style={{textAlign: "center"}}>Colors</h2>
+        <div style={Styles.colorSwatches}>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.Vibrant.getHex()}}></div>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.LightVibrant.getHex()}}></div>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.DarkVibrant.getHex()}}></div>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.Muted.getHex()}}></div>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.LightMuted.getHex()}}></div>
+          <div style={{width: "50px", height: "50px", backgroundColor: this.props.palette.DarkMuted.getHex()}}></div>
+        </div>
+        <h2 style={{textAlign: "center"}}>Tags</h2>
       </div>
     )
   }
